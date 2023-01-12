8d3814b90388495789d6eb4766938c2ed7b3d8d4
Wouter Diesveld <wouter@tinyqueries.com>
2023-01-12 15:36:44 +0100

    fixes missing requires

--- setup.py
+++ setup.py
@@ -4,10 +4,14 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name='tqc',
-    version='0.2.1',
+    version='0.2.2',
     entry_points = {
         'console_scripts': ['tqc=tqc.cli:main']
     },
+    install_requires=[
+        "requests",
+        "colorama"
+    ],
     author="Wouter Diesveld",
     author_email="wouter@querytechnology.com",
     description="Command line interface for the TinyQueries Compiler",
